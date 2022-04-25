8adaae85ba9a5a9f579f2be340a089280328a56e
Fernando Hernandez <fernandohruz@hotmail.com>
2022-04-25 12:43:31 -0500

    add librarian dashboard page which fetchs books from api

---
+++ src/pages/LibrarianDashboard.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import Book from './Book'
+const LibrarianDashboard = (props) => {
+    const [books, setBooks] = React.useState([]);
+    const fetchBooks = () => {
+        const url = "http://localhost:8080/api/book"
+        let options = {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Authorization": `Bearer ${props.jwt}`
+            }
+        }
+        return fetch(url, options);
+    }
+    useEffect(() => {
+        fetchBooks().then((res) =>
+            res.ok
+                ? res.json()
+                : Promise.reject({
+                    err: true,
+                    status: res.status || "00",
+                    statusText: res.statusText || "Ocurrió un error",
+                })
+        ).then(items => { setBooks(items) })
+            .catch((err) => err);
+    }, [])
+    return (
+        <div>
+            <button onClick={props.deleteJWT}>Log out</button>
+            <h1>Welcome !</h1>
+            {books.map(book => {
+                return (<Book key={book.id} title={book.title} author={book.author} isbn={book.isbn} />)
+            })}
+        </div>
+    )
+}
+
+export default LibrarianDashboard;
